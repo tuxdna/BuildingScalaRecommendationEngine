aacba4d3cdcd8126133c6b92e68513147e3c69d8
Saleem Ansari <tuxdna@gmail.com>
2015-07-14 07:02:00 +0530

    Added monthly trends.

--- webapp-recommender/app/util/Recommender.scala
+++ webapp-recommender/app/util/Recommender.scala
@@ -16,17 +16,25 @@ import com.twitter.algebird.TopCMS
 import reactivemongo.bson.BSONString
 
 object Recommender {
+  val useCMS = true
 
   def findTrendingCustomersFor(year: Int, month: Int): Future[List[String]] = {
     val cursor = ReactiveDB.oneMonthReviewsFrom(year, month)
     val enumerator = cursor.enumerate()
-    val itf: Iteratee[BSONDocument, Map[String, Long]] = Iteratee.fold(Map[String, Long]()) { (a, doc: BSONDocument) =>
-      val itemId = doc.get("customer").map { _.asInstanceOf[BSONString].value }.get
-      val voteCount = doc.get("votes").map { _.asInstanceOf[BSONDouble].value.toLong }.get
-      // println(s"itemId: $itemId, voteCount: $voteCount")
-      val newVotes = a.get(itemId).getOrElse(0L)
-      a + (itemId -> newVotes)
-    }
+    val itf: Iteratee[BSONDocument, Map[String, Long]] =
+      Iteratee.fold(Map[String, Long]()) {
+        (a, doc: BSONDocument) =>
+          val itemId = doc.get("customer").map {
+            _.asInstanceOf[BSONString].value
+          }.get
+
+          val voteCount = doc.get("votes").map {
+            _.asInstanceOf[BSONDouble].value.toLong
+          }.get
+
+          val newVotes = a.get(itemId).getOrElse(0L)
+          a + (itemId -> newVotes)
+      }
 
     val finalCounts = enumerator |>>> itf
     finalCounts.map { cms =>
@@ -36,25 +44,24 @@ object Recommender {
 
   def findTrendingProductsFor(year: Int, month: Int): Future[List[Long]] = {
     val cursor = ReactiveDB.oneMonthReviewsFrom(year, month)
-    // cursor.collect[Seq](100, false) foreach println
-
-    if (false) {
-      // Count Min Sketch parameters
-      val DELTA = 1E-3
-      val EPS = 0.01
-      val SEED = 1
-      val PERC = 0.001
+    if (useCMS) {
+      val DELTA = 1E-3; val EPS = 0.01; val SEED = 1; val PERC = 0.001
       val TOPK = 10 // K highest frequency elements to take
       val cms = TopPctCMS.monoid[Long](EPS, DELTA, SEED, PERC)
       val enumerator = cursor.enumerate()
-      val itf: Iteratee[BSONDocument, TopCMS[Long]] = Iteratee.fold(cms.zero) { (a, doc: BSONDocument) =>
-        val itemId = doc.get("productId").map { _.asInstanceOf[BSONDouble].value.toLong }.get
-        val voteCount = doc.get("votes").map { _.asInstanceOf[BSONDouble].value.toLong }.get
-        // println(s"itemId: $itemId, voteCount: $voteCount")
-        val b = cms.create(itemId)
-        val c = b + (itemId, voteCount)
-        a ++ c
-      }
+      val itf: Iteratee[BSONDocument, TopCMS[Long]] =
+        Iteratee.fold(cms.zero) {
+          (a, doc: BSONDocument) =>
+            val itemId = doc.get("productId").map {
+              _.asInstanceOf[BSONDouble].value.toLong
+            }.get
+            val voteCount = doc.get("votes").map {
+              _.asInstanceOf[BSONDouble].value.toLong
+            }.get
+            val b = cms.create(itemId)
+            val c = b + (itemId, voteCount)
+            a ++ c
+        }
       val finalCMS = enumerator |>>> itf
       finalCMS.map { cms =>
         cms.heavyHitters.toList
